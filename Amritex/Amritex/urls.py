9981aac95deb9aa2328f442bb2aa6fec447bbf76
Atul yadav <57094600+atul2501@users.noreply.github.com>
2026-08-01 11:11:45 +0530

    added more html code

--- Amritex/Amritex/urls.py
+++ Amritex/Amritex/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='Home'),
-    path('/about',views.about,name='About'),
-    path('/contact',views.contact,name="Contact"),
+    path('about/',views.about,name='About'),
+    path('contact/',views.contact,name="Contact"),
 
 ]
