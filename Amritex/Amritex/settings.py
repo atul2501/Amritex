18df9e13c6a8b64f287c23d3abb71b00babf3824
Atul yadav <57094600+atul2501@users.noreply.github.com>
2026-08-01 18:25:14 +0530

    website is ready

--- Amritex/Amritex/settings.py
+++ Amritex/Amritex/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tailwind'
+    'tailwind',
+    'theme'
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS=['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
